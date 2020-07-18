69cfe7eeec667057350adbddd75fa336a35f3cd0
dsilvasa <dsilvasa@cisco.com>
2020-07-18 16:32:06 +0100

    fix exponential scale

--- code/r/pdf_exponential.py
+++ code/r/pdf_exponential.py
@@ -7,22 +7,15 @@ from matplotlib import rc
 rc('text', usetex=True)
 
 total_divisions = 100000
-
 x = np.linspace(0, 5, total_divisions)
-y_05 = expon.pdf(x, 0, 0.5)
-y_1 = expon.pdf(x, 0, 1)
-y_15 = expon.pdf(x, 0, 1.5)
-y_3 = expon.pdf(x, 0, 3)
 
-plt.plot(x, y_05, label=r'$\lambda = 0.5$')
-plt.plot(x, y_1, label=r'$\lambda = 1$')
-plt.plot(x, y_15, label=r'$\lambda = 1.5$')
-plt.plot(x, y_3, label=r'$\lambda = 3$')
+for _lambda in [0.5, 1, 1.5, 3]:
+    # See https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.expon.html for scale param
+    plt.plot(x, expon.pdf(x, 0, 1/_lambda), label=f'$\\lambda = {_lambda}$')
+
 plt.xlabel(r'$x$')
 plt.ylabel(r'$f(x)$')
 plt.legend()
-
-# plt.show()
 
 # Save file. This counts on the working directory to be the "code" folder !!!
 plt.savefig('../doc/images/r/exponential/pdf.pdf')
