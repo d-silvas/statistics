4b5eeace2783899e00f500ce309475825c75181c
dsilvasa <dsilvasa@cisco.com>
2020-07-22 08:44:33 +0100

    fix discrete cdfs plots

--- code/discrete/cdf_binomial.py
+++ code/discrete/cdf_binomial.py
@@ -1,6 +1,7 @@
 from scipy.stats import binom
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from matplotlib import rc
 
 rc('text', usetex=True)
@@ -14,15 +15,19 @@ params = [
     { 'n': 100, 'p': 0.25 },
 ]
 
+default_cycle_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 # Separate plots
 for nAndP in params:
     n = nAndP['n']
     p = nAndP['p']
 
-    x = np.linspace(0, n, n + 1)
-    y = binom.cdf(x, n, p)
+    x_integers = range(n)
 
-    plt.bar(x, y)
+    for x in x_integers:
+        y = binom.cdf(x, n, p)
+        plt.scatter(x, y, c=default_cycle_colors[0])
+        plt.plot([x, x + 1], [y, y], color=default_cycle_colors[0])
 
     # Save file. This counts on the working directory to be the "code" folder !!!
     pStr = str(p).replace('.', '')
@@ -30,14 +35,21 @@ for nAndP in params:
     plt.close()
 
 # Joint plot
-for nAndP in params:
+handles = [] # For the legend
+for index, nAndP in enumerate(params):
     n = nAndP['n']
     p = nAndP['p']
 
-    x = np.linspace(0, 40, 41) # Set max x to plot to 40
-    y = binom.cdf(x, n, p)
+    x_integers = range(40) # Set max x to plot to 40
 
-    plt.bar(x, y, label=f'$n = {n}, p = {p}$')
+    color = default_cycle_colors[index]
 
-plt.legend()
+    for x in x_integers:
+        y = binom.cdf(x, n, p)
+        plt.scatter(x, y, c=color)
+        plt.plot([x, x + 1], [y, y], color=color)
+
+    handles.append(mpatches.Patch(color=color, label=f'$n = {n}, p = {p}$'))
+
+plt.legend(handles=handles)
 plt.savefig('../doc/images/discrete/binomial/cdf_all.pdf')
--- code/discrete/cdf_poisson.py
+++ code/discrete/cdf_poisson.py
@@ -1,6 +1,8 @@
+
 from scipy.stats import poisson
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from matplotlib import rc
 
 rc('text', usetex=True)
@@ -8,22 +10,32 @@ rc('text', usetex=True)
 max_num = 60
 means = [2, 3, 5, 10, 20, 30]
 
+default_cycle_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 # Separate plots
 for mean in means:
-    x = range(max_num)
-    y = poisson.cdf(x, mean)
+    x_integers = range(max_num)
 
-    plt.bar(x, y)
+    for x in x_integers:
+        y = poisson.cdf(x, mean)
+        plt.scatter(x, y, c=default_cycle_colors[0])
+        plt.plot([x, x + 1], [y, y], color=default_cycle_colors[0])
 
     plt.savefig(f'../doc/images/discrete/poisson/cdf_{mean}.pdf')
     plt.close()
 
 # Joint plot
-for mean in means:
-    x = range(max_num)
-    y = poisson.cdf(x, mean)
+handles = []
+for index, mean in enumerate(means):
+    x_integers = range(max_num)
+    color = default_cycle_colors[index]
 
-    plt.bar(x, y, label=f'$\\lambda = {mean}$')
+    for x in x_integers:
+        y = poisson.cdf(x, mean)
+        plt.scatter(x, y, c=color)
+        plt.plot([x, x + 1], [y, y], color=color)
 
-plt.legend()
+    handles.append(mpatches.Patch(color=color, label=f'$\\lambda = {mean}$'))
+
+plt.legend(handles=handles)
 plt.savefig('../doc/images/discrete/poisson/cdf_all.pdf')
